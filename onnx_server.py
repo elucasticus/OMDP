dbda9acae7ee72cb2f4a978c6fc242e29e4e6fa7
elucasticus <luca7.crippa@mail.polimi.it>
2023-02-24 13:35:08 +0100

    split during run

--- onnx_server.py
+++ onnx_server.py
@@ -5,8 +5,9 @@ from onnx_second_inference_flask import onnx_search_and_run_second_half
 from onnx_third_inference_flask import onnx_search_and_run_third_half
 import numpy as np
 import json
+import onnx
 
-def run(onnx_path, EP_list, device):
+def run(onnx_file, EP_list, device):
     app = Flask(__name__)
 
     @app.route("/")
@@ -19,10 +20,26 @@ def run(onnx_path, EP_list, device):
 
     @app.route("/endpoint", methods=["POST", "GET"])
     def endpoint():
+        #Receive the incoming data
         data = json.load(request.files['data'])
         data["result"] = np.load(request.files['document']).tolist()
+
+        #Compute arrival time 
         arrival_time = time.time()
-        returnData = onnx_search_and_run_third_half(onnx_path, None, data, None, EP_list, device)
+
+        #Split the model to obtain the third submodel
+        onnx_model_file = "temp/third_half.onnx"
+        input_names = [data["splitLayer"]]
+        output_names = []
+        onnx_model = onnx.load(onnx_file)
+        for i in range(len(onnx_model.graph.output)):
+            output_names.append(onnx_model.graph.output[i].name)
+        onnx.utils.extract_model(onnx_file, onnx_model_file, input_names, output_names)
+
+        #Compute the time needed to run the third submodel
+        returnData = onnx_search_and_run_third_half(None, onnx_model_file, data, None, EP_list, device)
+
+        #Return the results
         returnData["Outcome"] = "Success"
         returnData["arrival_time"] = arrival_time
         returnData["result"] = returnData["result"].tolist()
@@ -30,15 +47,29 @@ def run(onnx_path, EP_list, device):
 
     @app.route("/checkpoint", methods=["POST", "GET"])
     def checkpoint():
+        #Receive the incoming data
         data = json.load(request.files['data'])
         data["result"] = np.load(request.files['document']).tolist()
-        data = onnx_search_and_run_second_half(onnx_path, None, data, None, EP_list, device)
+
+        #TEST
+        name = "sequential/mobilenetv2_1.00_160/block_15_add/add:0"
+
+        #Split the model to obtain the second submodel
+        onnx_model_file = "temp/second_half.onnx"
+        input_names = [data["splitLayer"]]
+        output_names = [name]
+        onnx.utils.extract_model(onnx_file, onnx_model_file, input_names, output_names)
+
+        #Compute the time needed to run the second submodel
+        data = onnx_search_and_run_second_half(None, onnx_model_file, data, None, EP_list, device)
         np.save("input_check", data["result"])
         del data["result"]
+        data["splitLayer"] = name
         files = [
             ('document', ("input_check.npy", open("input_check.npy", 'rb'), 'application/octet')),
             ('data', ('data', json.dumps(data), 'application/json')),
         ]
+
         #Send the Intermediate Tensors to the server
         print("Sending the intermediate tensors to the server...")
         server_url = "http://127.0.0.1:3000/endpoint"
