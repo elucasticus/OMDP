dd00002466ecc23db6783437911e76e505beb639
elucasticus <luca7.crippa@mail.polimi.it>
2023-03-02 14:22:08 +0100

    execution providers

--- onnx_server.py
+++ onnx_server.py
@@ -53,14 +53,16 @@ def onnx_get_true_inputs(onnx_model):
 @click.option("--onnx_file", help="Select the ONNX file to use for the inference")
 @click.option("--server_url", default="", help="Set the url of the next device on the chain. If this is the endpoint, use an empyt string")
 @click.option("--log_file", default="", help="Select where to save the log of the operation performed")
-@click.option("--EP_list", "EP_list", default="CPU", help="Select the Execution Provider used at inference (CPU (default) | GPU | OpenVINO | TensorRT | ACL)")
+@click.option("--exec_provider", default="CPU", help="Select the Execution Provider used at inference (CPU (default) | GPU | OpenVINO | TensorRT | ACL)")
 @click.option("--device", default=None, help="Specify the device type such as 'CPU_FP32', 'GPU_FP32', 'GPU_FP16', etc..")
 @click.option("--threshold", default=10., help="Specify the threshold above which we skip the iteration")
 @click.option("--port", default=5000, help="Select the port where to run the flask app")
 @click.option("--host", default="127.0.0.1", help="Select where to host the flask app")
-def main(onnx_file, server_url, log_file, EP_list, device, threshold, port, host):
+def main(onnx_file, server_url, log_file, exec_provider, device, threshold, port, host):
     shutil.rmtree("cache")
-    if EP_list == "CPU":
+    if exec_provider == "GPU":
+       EP_list = ["CUDAExecutionProvider"]
+    else:
        EP_list = ["CPUExecutionProvider"]
     app = run(onnx_file, server_url, log_file, EP_list, device, threshold)
     app.run(port=port, host=host)
